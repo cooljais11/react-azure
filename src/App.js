c5c4365b84042c66e3204d64910287a02555dd4a
cooljais11 <25701919+cooljais11@users.noreply.github.com>
2019-10-06 17:19:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>DICE Hackers are coming !!! </p>
-          <p>2 days to go !!</p>
+          <p>2 days to go !!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
